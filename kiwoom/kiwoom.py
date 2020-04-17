474bdf3139de4d6253e362c2a70c15b8e77a2264
username <user@email.mail>
2020-04-18 06:40:59 +0900

    first

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,20 +1,25 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 from PyQt5.QAxContainer import *
 
-class Kiwoom(QMainWindow):
+class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__()
-        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
-        self.kiwoom.dynamicCall("CommConnect()")
-        self.kiwoom.OnEventConnect.connect(self.login_event)
 
-    def login_event(self, error):
-        if error == 0:
-            strs = '로그인 성공 Code : ' + str(error)
-            self.statusBar().showMessage(strs)
-        else:
-            strs = '로그인 실패 Code : ' + str(error)
+        self.get_instance()
+        self.event_slot()
+
+        self.signal_login()
+
+    def get_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def event_slot(self):
+        self.OnEventConnect.connect(self.login_slot)
+
+    def login_slot(self, errCode):
+        print(errCode)
+
+    def signal_login(self):
+        self.dynamicCall("CommConnect()")
 
 
 
